957073603841c5dfc219527179bc901c23dbfbea
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-08 17:58:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"programming is like building a muiltilingual puzzle");
 	return (0);
 }
