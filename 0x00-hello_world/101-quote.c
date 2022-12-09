46e423557349ec6300cb0589c691f3c64c28f118
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-08 22:10:35 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 
 /**
  * main - Entry point
- *
- * Return: Always i
+ * Return: Always 1
  */
 int main(void)
-{
-	fprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
