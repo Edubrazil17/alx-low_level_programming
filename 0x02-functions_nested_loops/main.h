3b5dd7b62c43cfb35c1ba5ef443060f157034f09
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-13 14:56:59 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+/**
+
+ * _putchar - prints input using standard library
+
+ * @c: The character to print
+
+ * Return: 1 is success
+
+ *
+
+ * print_alphabet - prints all the alphabets in lowercase
+
+ *
+ */
+
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+
+
+#endif
