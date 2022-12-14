cb6aeee985766f72a8d4cf724ddcedbf4b47e21c
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-14 06:34:52 -0800

    main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,9 +3,7 @@
 #define MAIN_H
 
 
-
 /**
-
  * _putchar - prints input using standard library
 
  * @c: The character to print
@@ -16,7 +14,7 @@
 
  * print_alphabet - prints all the alphabets in lowercase
 
- *
+ * print_alphabet_x10 - prints all the alphabets in lowercase ten times
  */
 
 
@@ -25,6 +23,27 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
 
 
 #endif
