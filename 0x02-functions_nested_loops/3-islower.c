daae7a9cff1e404673f47be3edda3130ca0260ba
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-14 09:28:02 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * _islower - this is the entry point of the program
+ *
+ * @input: its takes in chat
+ *
+ * Description: this function returns 1 or 0
+ *
+ * Return: 1 is true and 0 is false
+*/
+
+
+
+int _islower(int input)
+
+{
+
+	if (input >= 97 && input <= 122)
+
+	{
+
+		return (1);
+
+	}
+
+
+
+	return (0);
+
+}
