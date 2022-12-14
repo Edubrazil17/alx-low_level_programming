c8fd1ca2e4098e4a13a7e1e3201c0301c584c04b
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-14 15:51:10 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - Entry point
+* @a: the first parameter
+* @b: the second parameter
+* Description: this function return 1 or 0
+*Return: 1 (success)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+
+}
