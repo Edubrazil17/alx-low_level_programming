0bdf1fa0f5a6d62fc3e633fab2d594661a0dc4c3
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-14 18:37:09 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - this is the entry point of the program 
+* Description: this function return 1 or 0
+* Return: 0
+*/
+int main(void)
+{
+	unsigned long a = 0, b = 1, iterator = 0, sum = 0;
+
+	while (iterator <= 4000000)
+	{
+		iterator = a + b;
+
+		a = b;
+
+		b = iterator;
+
+		if ((a % 2) == 0)
+			sum += a;
+	}
+	printf("%ld\n", sum);
+	return (0);
+
+}
