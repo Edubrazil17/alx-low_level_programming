09cb831d8b4deff47ea1a3667ad12e698e85f7d3
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-16 15:25:25 -0800

    Numbers have life; they're not just symbols on pape

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,11 +11,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num = num;
+		num = -num;
 	}
 
 	if (num > 9)
-		print_number(num / 100);
+		print_number(num / 10);
 
 	_putchar(num % 10 + '0');
 }
