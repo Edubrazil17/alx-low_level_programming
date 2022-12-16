4e7d4dfdb59d8ca8b0c6bce75b0a551a6e4fc211
Edubrazil17 <chinedu.eze009@gmail.com>
2022-12-16 00:05:27 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,10 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 
-		_putchar(num);
+		_putchar(num + '0');
 
-	_putchar(9);
+	_putchar(10);
 
 }
