77d3362857141be7affc7ab40ac728b6bf1ff3fd
Edubrazil17 <chinedu.eze009@gmail.com>
2023-01-05 07:45:48 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,22 @@
- #include "main.h"
+#include "main.h"
 
-/** 
- *  main - entry point
- *
- *  _puts_recursion -a function that call itself
- *  *s - string
- *  Return: nothing
- *
+/**
+ * _puts_recursion - a function that call itself
+ * @s - string
+ * Return: nothing
  */
 
-void _puts_recursion(char *s)
-
+void _puts_recursion(char *s);
 {
+
 	if (*s != '\0')
-
+	
 	{
-
-	_putchar(*s);
-
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		
+		_puts_recursion(s + 1);
 
 	}
 
 	_putchar('\n');
-
-
 }
